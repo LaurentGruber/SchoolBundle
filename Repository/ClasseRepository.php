96f3912b8239a31d5a6618da453b990d23e99f1c
LaurentG <laurent@sk1.be>
2014-11-12 17:32:05 +0100

    classe repo

--- Repository/ClasseRepository.php
+++ Repository/ClasseRepository.php
@@ -8,14 +8,15 @@ use Claroline\CoreBundle\Entity\User;
 class ClasseRepository extends EntityRepository
 {
     public function findUserClasse(User $user){
+        $em = $this->getEntityManager();
         /* ManyToMany à la place de onetomany car je ne modifie pas l'entity user du coreBundle */
         $dql ="SELECT c
          FROM Laurent\SchoolBundle\Entity\Classe c
          JOIN c.eleves e
-         Where e = :userId";
-        $query = $this->_em->createQuery($dql);
+         Where e.id = :userId";
+        $query = $em->createQuery($dql);
         $query->setParameter('userId', $user->getId());
 
-        return $query->getResult();
+        return $query->getOneOrNullResult();
     }
 }
